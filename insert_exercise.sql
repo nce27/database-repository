793fca7db28e9dad6deae17debee3ed797923688
nce27 <nc3elliott@gmail.com>
2023-05-22 14:51:34 +0100

    Inserted a new artist by name and genre. Then inserted an album by title and release year.

--- insert_exercise.sql
+++ insert_exercise.sql
@@ -6,6 +6,21 @@
 --   ( name, genre )
 --   VALUES( 'Massive Attack', 'Alternative' );
 
-INSERT INTO albums
-  ( title, release_year )
-  VALUES( 'Mezzanie', '1998')
+
+
+-- Creates a new album called Mezzanie, released in 1998
+-- Update statement used to set the artist id to 5 in the 13th numeric id (the new album)
+
+-- INSERT INTO albums
+--   ( title, release_year )
+--   VALUES( 'Mezzanie', '1998');
+
+-- UPDATE albums SET artist_id = '5' WHERE id = '13';
+
+INSERT INTO artists
+  (name, genre)
+  VALUES ( 'King Krule', 'Alt-Jazz' );
+
+INSERT INTO albums 
+  ( title, release_year)
+  VALUES( 'The Ooz', '2017');

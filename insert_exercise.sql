81da316f5022ec63ddcf21e5e1c8d01bb4120c5b
nce27 <nc3elliott@gmail.com>
2023-05-22 14:36:34 +0100

    Updated to complete exercise to insert new record in albums and use UPDATE query to update the artist ID of that new album.

--- insert_exercise.sql
+++ insert_exercise.sql
@@ -1,4 +1,11 @@
+-- INSERT INTO [table name]
+--   ( [list of columns] )
+--   VALUES( [list of values] );
 
-INSERT INTO artists
-  ( name, genre )
-  VALUES( 'Massive Attack', 'Alternative' );
+-- INSERT INTO artists
+--   ( name, genre )
+--   VALUES( 'Massive Attack', 'Alternative' );
+
+INSERT INTO albums
+  ( title, release_year )
+  VALUES( 'Mezzanie', '1998')
